98b465524e1b69857c6dc8383bb10da6c56011cf
Mohit <sopranosmohit@gmail.com>
2019-09-23 22:57:06 +0530

    Complete Services without Circuit Breakers

---
+++ src/main/java/com/example/ratingdataservice/RatingResource.java
@@ -0,0 +1,26 @@
+package com.example.ratingdataservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+    @RequestMapping("/movie/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
+
